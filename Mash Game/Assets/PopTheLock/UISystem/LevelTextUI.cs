9cad1e77c75a75711842d639b4c91c7a6fe94c81
Lextord78 <102995012+Lextord78@users.noreply.github.com>
2023-10-25 00:31:08 -0400

    Start Over
    Starting over with the code.

--- Mash Game/Assets/PopTheLock/UISystem/LevelTextUI.cs
+++ Mash Game/Assets/PopTheLock/UISystem/LevelTextUI.cs
@@ -1,18 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelTextUI : MonoBehaviour
 {
-    public GameData GameData;
-    TMPro.TextMeshProUGUI _text;
 
-    void Start()
-    {
-        _text = GetComponent<TMPro.TextMeshProUGUI>();
-        _text.text = "Level: " + GameData.CurrentLevel.ToString();
-    }
-
-    void Update()
-    {
-        _text.text = "Level: " + GameData.CurrentLevel.ToString();
-    }
 }
