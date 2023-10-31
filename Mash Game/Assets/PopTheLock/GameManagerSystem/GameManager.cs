4c3fe9404d407209b4bab4ff21cbd8d75c16ec0b
Lextord78 <102995012+Lextord78@users.noreply.github.com>
2023-10-31 00:13:42 -0400

    Adding isfirsttap and removing SUD
    Added the isFirstTap to the gamemananger.cs and remove the ScoreUpDate.cs as it not needed anymore.

--- Mash Game/Assets/PopTheLock/GameManagerSystem/GameManager.cs
+++ Mash Game/Assets/PopTheLock/GameManagerSystem/GameManager.cs
@@ -39,7 +39,7 @@ public class GameManager : MonoBehaviour
     {
         GameData.CurrentLevel++;
         GameData.resetLevel();
-        //isFirstTap = true;
+        isFirstTap = true;
     }
     public void loadSameLevel()
     {
