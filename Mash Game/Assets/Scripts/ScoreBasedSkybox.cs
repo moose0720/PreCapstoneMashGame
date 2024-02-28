67d6f65c15bbdb76972d3abfa5abc9bd8c4ea65a
Lextord78 <102995012+Lextord78@users.noreply.github.com>
2024-02-28 09:24:24 -0500

    save point

--- Mash Game/Assets/Scripts/ScoreBasedSkybox.cs
+++ Mash Game/Assets/Scripts/ScoreBasedSkybox.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class ScoreBasedSkybox : MonoBehaviour
 {
-    /*public MSCounter MScore;
+   /* public MSCounter MScore;
     public Material[] skyboxes;
     private int currentSkyBoxIndex = 0;
     // Start is called before the first frame update
@@ -14,8 +14,8 @@ public class ScoreBasedSkybox : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        int Score = MScore.addMashScore();
-        if(MScore >= 20 && currentSkyBoxIndex == 0) // Change to the second skybox when score reaches 20
+        MScore.addMashScore; // Get score from MSCounter
+        if (MScore >= 20 && currentSkyBoxIndex == 0) // Change to the second skybox when score reaches 20
         {
             currentSkyBoxIndex = 1;
             RenderSettings.skybox = skyboxes[currentSkyBoxIndex];
