797d001a4f966154b3357937b14f4b052dfd1fe4
Lextord78 <102995012+Lextord78@users.noreply.github.com>
2024-02-29 01:33:24 -0500

    save point on skybox
    A little stuck new errors have appeared.

--- Mash Game/Assets/Scripts/ScoreBasedSkybox.cs
+++ Mash Game/Assets/Scripts/ScoreBasedSkybox.cs
@@ -1,24 +1,30 @@
 using UnityEngine;
 namespace Assets.Scripts
-public class ScoreBasedSkybox : MonoBehaviour
 {
-    public MSCounter MScore;
-    public Material[] skyboxes;
-    private int currentSkyBoxIndex = 0;
-    // Start is called before the first frame update
-    void Start()
+    public class ScoreBasedSkybox : MonoBehaviour
     {
-        RenderSettings.skybox = skyboxes[currentSkyBoxIndex];
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        MScore.addMashScore; // Get score from MSCounter
-        if (MScore >= 20 && currentSkyBoxIndex == 0) // Change to the second skybox when score reaches 20
+        public  MSCounter MScore;
+        public Material[] skyboxes;
+        private int currentSkyBoxIndex = 0;
+        // Start is called before the first frame update
+        void Start()
         {
-            currentSkyBoxIndex = 1;
             RenderSettings.skybox = skyboxes[currentSkyBoxIndex];
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(MScore != null)
+            {
+                MScore.addMashScore(); // Get score from MSCounter
+
+            }
+            //if (MScore >= 20 && currentSkyBoxIndex == 0) // Change to the second skybox when score reaches 20
+            {
+                currentSkyBoxIndex = 1;
+                RenderSettings.skybox = skyboxes[currentSkyBoxIndex];
+            }
         }
     }
 }
