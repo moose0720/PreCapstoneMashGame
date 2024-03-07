d33054690009542f89ab8dc91f74f86079060fd5
Lextord78 <102995012+Lextord78@users.noreply.github.com>
2024-03-07 09:07:29 -0500

    stand in
    This CS will not be use unless need to.

---
+++ Mash Game/Assets/Scripts/LetterAndWords.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+   /* public class LetterAndWords : MonoBehaviour
+    {
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }*/
+}
